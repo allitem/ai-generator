bdcc1911e495887f67e05c628719a2e97f9946ff
Rufio Dinoto(Musk) <244thanva@gmail.com>
2026-02-05 11:43:20 +0700

    Create DraggableComponent.jsx

---
+++ frontend/src/DraggableComponent.jsx
@@ -0,0 +1,24 @@
+import { useDrag } from "react-dnd";
+
+export default function DraggableComponent({ type }) {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: "COMPONENT",
+    item: { type },
+    collect: (monitor) => ({ isDragging: !!monitor.isDragging() })
+  }));
+
+  return (
+    <div
+      ref={drag}
+      style={{
+        opacity: isDragging ? 0.5 : 1,
+        border: "1px solid #888",
+        margin: "5px",
+        padding: "5px",
+        cursor: "move"
+      }}
+    >
+      {type}
+    </div>
+  );
+}
